3bac4820e5c99377ff37c6fc8470eb8501f79b96
Dondre Mclenan <d.mclenan@gmail.com>
2022-03-02 19:21:04 -0500

    3rd

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -32,6 +32,6 @@ export const getDb = async () => {
   const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 initdb();
